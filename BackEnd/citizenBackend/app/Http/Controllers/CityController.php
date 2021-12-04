cfbb5e8bf41bda3e6a618d5044f0caa16a75def2
Hai SOn <warcratf32001@gmail.com>
2021-12-04 20:37:31 +0700

    nghich dai

---
+++ BackEnd/citizenBackend/app/Http/Controllers/CityController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\city;
+use Illuminate\Http\Request;
+
+class CityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\city  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function show(city $city)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\city  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(city $city)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\city  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, city $city)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\city  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(city $city)
+    {
+        //
+    }
+}
--- BackEnd/citizenBackend/routes/web.php
+++ BackEnd/citizenBackend/routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function (Request $request) {
+    
     return view('welcome');
 });
