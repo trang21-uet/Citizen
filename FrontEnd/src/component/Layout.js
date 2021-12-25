7034d54d6a415ad0b54aa46ec8caf6425d5587eb
Hai SOn <warcratf32001@gmail.com>
2021-12-26 01:02:44 +0700

    footer

--- FrontEnd/src/component/Layout.js
+++ FrontEnd/src/component/Layout.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { ProtectedRoute } from "../auth/AuthProvider";
 import Header from "../component/shared/Header";
+import Footer from "../component/shared/Footer";
 import { Tooltip } from "bootstrap/dist/js/bootstrap.bundle.js";
 
 const Layout = () => {
@@ -28,6 +29,7 @@ const Layout = () => {
     <ProtectedRoute>
       <Header></Header>
       <Outlet></Outlet>
+      <Footer></Footer>
     </ProtectedRoute>
   );
 };
---
+++ FrontEnd/src/component/shared/Footer.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer class="py-3 my-4">
+        <p class="text-center text-muted">Bài tập lớn môn thiết kế ứng dụng web</p>
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <div class="row g-0">
+                <div class="col-6">
+                    <div class="p-1 border-end text-end">
+                        Nguyễn Hải Sơn
+                        <i class="bi bi-person-badge ps-2"></i>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="p-1 border-start">19020424</div>
+                </div>
+                <div class="col-6">
+                    <div class="p-1 border-end text-end">
+                        Nguyễn Xuân Trang
+                        <i class="bi bi-person-badge ps-2"></i>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="p-1 border-start">19020464</div>
+                </div>
+                <div class="col-6">
+                    <div class="p-1 border-end text-end">
+                        Triệu Minh Tiến
+                        <i class="bi bi-person-badge ps-2"></i>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="p-1 border-start">19020169</div>
+                </div>
+            </div>
+        </ul>
+    </footer>
+  );
+};
+
+export default Footer;
