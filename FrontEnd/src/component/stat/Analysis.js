028ebb79d553dd657342f362f0f2fe24034e0f79
Nguyen Xuan Trang <tranthudoan2036@gmail.com>
2021-12-26 18:34:00 +0700

    Update Analysis.js

--- FrontEnd/src/component/stat/Analysis.js
+++ FrontEnd/src/component/stat/Analysis.js
@@ -2,15 +2,17 @@ import React, { useEffect, useState } from "react";
 import { useAuth } from "../../auth/AuthProvider";
 import { RadioGroup } from "../shared/InputGroup";
 import { Chart } from "react-google-charts";
+import InfoGroup from "../shared/InfoGroup";
 
 const Analysis = ({ data }) => {
-  const [info, setInfo] = useState([]);
-  const [chilren, setChildren] = useState([]);
-  const [age, setAge] = useState([]);
+  const [content, setContent] = useState([]);
+  const [children, setChildren] = useState({});
   const auth = useAuth();
-  const childName = {
-    A1: "maTinh",
-    A2: "maHuyen",
+  const child = {
+    A1: ["maTinh", "tenTinh"],
+    A2: ["maHuyen", "tenHuyen"],
+    A3: ["maXa", "tenXa"],
+    B1: ["maThon", "tenThon"],
   };
 
   const request = async () => {
@@ -26,67 +28,29 @@ const Analysis = ({ data }) => {
         if (data.error) {
           throw data.error;
         } else {
-          console.log(data);
-          let child = [];
+          // console.log(data);
+          let childName = {};
           data.forEach((element) => {
-            child.push(element[childName[auth.info().type]]);
+            childName[element[child[auth.info().type][0]]] =
+              element[child[auth.info().type][1]];
           });
-          setChildren(child);
+          // console.log(childName);
+          setChildren({ ...childName });
         }
       })
       .catch((error) => console.log(error));
   };
 
-  useEffect(() => ["A1", "A2"].includes(auth.info().type) && request(), []);
+  useEffect(() => auth.info().type !== "B1" && request(), []);
 
   const handleChange = (event) => {
     const field = event.target.value;
     if (field === "place") {
-      if (["A1", "A2"].includes(auth.info().type)) {
-      } else {
-      }
+      setContent(<PopulationChart data={data} children={children} />);
     } else {
+      setContent(<AgeChart data={data} />);
     }
   };
-
-  const treCon = (data) => {
-    var temp = [];
-    var today = new Date();
-    data.forEach(person => {
-      var birthDate = new Date(person.ngaySinh);
-      var age = today.getFullYear() - birthDate.getFullYear();
-      if (age <= 14) {
-        temp.push(person);
-      }
-    });
-    return temp.length;
-  }
-
-  const nguoiLaoDong = (data) => {
-    var temp = [];
-    var today = new Date();
-    data.forEach(person => {
-      var birthDate = new Date(person.ngaySinh);
-      var age = today.getFullYear() - birthDate.getFullYear();
-      if (age > 14 && age <= 59) {
-        temp.push(person);
-      }
-    });
-    return temp.length;
-  }
-
-  const nguoiGia = (data) => {
-    var temp = [];
-    var today = new Date();
-    data.forEach(person => {
-      var birthDate = new Date(person.ngaySinh);
-      var age = today.getFullYear() - birthDate.getFullYear();
-      if (age > 60) {
-        temp.push(person);
-      }
-    });
-    return temp.length;
-  }
 
   return (
     <div className="border-top py-3">
@@ -97,7 +61,6 @@ const Analysis = ({ data }) => {
         id="address"
         value="place"
         label="Nơi khai báo"
-        checked={true}
         onChange={handleChange}
       />
       <RadioGroup
@@ -107,27 +70,111 @@ const Analysis = ({ data }) => {
         label="Độ tuổi"
         onChange={handleChange}
       />
-
-
-      {/* độ tuổi */}
-      <Chart
-        class="mx-auto"
-        width={'900px'}
-        height={'500px'}
-        chartType="PieChart"
-        loader={<div>Đang tải biểu đồ</div>}
-        data={[
-          ['Độ tuổi', 'Age'],
-          ['0 - 14', treCon(data)],
-          ['15 - 59', nguoiLaoDong(data)],
-          ['> 60', nguoiGia(data)],
-        ]}
-        options={{
-          title: 'Biểu đồ về độ tuổi của người dân',
-        }}
-        rootProps={{ 'data-testid': '1' }}
-      />
+      {content}
     </div>
+  );
+};
+
+const PopulationChart = ({ data, children }) => {
+  const [info, setInfo] = useState();
+  const auth = useAuth();
+
+  let counter = [];
+  let result = [];
+  Object.keys(children).forEach((id) => {
+    counter = [];
+    counter = data.map((person) => {
+      if (auth.info().type === "B1") {
+        if (person.B2 === id) {
+          return 1;
+        }
+      } else if (person.B1.slice(0, id.length) === id) {
+        return 1;
+      }
+      return 0;
+    });
+    result.push([children[id], counter.filter((x) => x === 1).length]);
+  });
+
+  useEffect(() => {
+    console.log(result);
+    setInfo(result);
+  }, []);
+
+  return (
+    <Chart
+      class="mx-auto"
+      width={"900px"}
+      height={"500px"}
+      chartType="PieChart"
+      loader={<div>Đang tải biểu đồ</div>}
+      data={info}
+      options={{
+        title: "Biểu đồ dân số của từng vùng",
+      }}
+      rootProps={{ "data-testid": "1" }}
+    />
+  );
+};
+
+const AgeChart = ({ data }) => {
+  const treCon = (data) => {
+    var temp = [];
+    var today = new Date();
+    data.forEach((person) => {
+      var birthDate = new Date(person.ngaySinh);
+      var age = today.getFullYear() - birthDate.getFullYear();
+      if (age <= 14) {
+        temp.push(person);
+      }
+    });
+    return temp.length;
+  };
+
+  const nguoiLaoDong = (data) => {
+    var temp = [];
+    var today = new Date();
+    data.forEach((person) => {
+      var birthDate = new Date(person.ngaySinh);
+      var age = today.getFullYear() - birthDate.getFullYear();
+      if (age > 14 && age <= 59) {
+        temp.push(person);
+      }
+    });
+    return temp.length;
+  };
+
+  const nguoiGia = (data) => {
+    var temp = [];
+    var today = new Date();
+    data.forEach((person) => {
+      var birthDate = new Date(person.ngaySinh);
+      var age = today.getFullYear() - birthDate.getFullYear();
+      if (age > 60) {
+        temp.push(person);
+      }
+    });
+    return temp.length;
+  };
+
+  return (
+    <Chart
+      class="mx-auto"
+      width={"900px"}
+      height={"500px"}
+      chartType="PieChart"
+      loader={<div>Đang tải biểu đồ</div>}
+      data={[
+        ["Độ tuổi", "Age"],
+        ["0 - 14", treCon(data)],
+        ["15 - 59", nguoiLaoDong(data)],
+        ["> 60", nguoiGia(data)],
+      ]}
+      options={{
+        title: "Biểu đồ về độ tuổi của người dân",
+      }}
+      rootProps={{ "data-testid": "1" }}
+    />
   );
 };
 
